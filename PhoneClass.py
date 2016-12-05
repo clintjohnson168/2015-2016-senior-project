74f647957e10fa447df9ea88575eb2b236165eab
clintjohnson168 <dewman_92@hotmail.com>
2016-12-05 09:38:23 -0600

    Updated the Phone class.
    Added Sprint and US Cellular as valid phone providers.

--- PhoneClass.py
+++ PhoneClass.py
@@ -1,19 +1,29 @@
+'''
+This is a class that creates phone opject with a phone number,
+cell provider, and a correct email to that phone number. 
+'''
 
 class Phone:
+    #constructer using phone number and cell provider
     def __init__(self, num, provider):
         self.number = num
         self.provider = provider
         self.email = self.generate_email()
 
+    '''
+    This function will provide the correct email address
+    based on the cell provider information.
+    Will return an empty string if the cell provider is invalid. 
+    '''
     def generate_email(self):
         if(self.provider == "T-Mobile"):
             return "+1" + self.number + "@tmomail.net"
-        if(self.provider == "Verizon"):
+        elif(self.provider == "Verizon"):
             return self.number + "@vtext.com"
-        if(self.provider == "AT&T"):
+        elif(self.provider == "AT&T"):
             return self.number + "@mms.att.net"
-        if(self.provider == "Sprint"):
+        elif(self.provider == "Sprint"):
             return self.number + "@messaging.sprintpcs.com"
-        if(self.provider == "US Cellular"):
+        elif(self.provider == "US Cellular"):
             return self.number + "@email.uscc.net"
-        return "Can't generate email."
+        return ""
